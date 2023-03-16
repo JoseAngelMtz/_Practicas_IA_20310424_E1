c270370cc65f72275e7b4a56449748ec64bb650d
JoseAngelMtz <125079977+JoseAngelMtz@users.noreply.github.com>
2023-03-16 10:31:22 -0600

    004_JoseAngelMartinezGarnica_20310424
    se cambiaron los nombres de los archivos para un mejor orden

---
+++ _2023_03_16_002_Variables_v1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 10:11:55 2023
+
+@author: josea
+"""
+
+"capitulo 2"
+mensaje = "hola"
+x= 10
+y= 54
+mensaje =x+y
+print(mensaje)
---
+++ _2023_03_16_019-020_tuplas_v1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 10:23:24 2023
+
+@author: josea
+"""
+
+"capitulo 19"
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print(colores[1])
+
+numeros = (10, 1, 5, 11)
+operacion= numeros[0] - numeros[1] + numeros[2] + numeros[3]
+print(operacion)
+
